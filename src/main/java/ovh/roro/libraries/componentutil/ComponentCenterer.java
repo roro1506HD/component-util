d71709066b3e1dd1fb16c4e48399e62965de4a0e
roro1506HD <roro1506mcpro@gmail.com>
2025-08-30 01:46:22 +0200

    Fixed color and newline handling in ComponentCenterer

--- src/main/java/ovh/roro/libraries/componentutil/ComponentCenterer.java
+++ src/main/java/ovh/roro/libraries/componentutil/ComponentCenterer.java
@@ -1,123 +1,82 @@
 package ovh.roro.libraries.componentutil;
 
 import com.google.common.base.Preconditions;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.ComponentContents;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.contents.PlainTextContents;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 class ComponentCenterer {
 
     private static final @NotNull LazyGlyphWidthSupplier SPACE_WIDTH = new LazyGlyphWidthSupplier(Component.literal(" "));
 
     private @Nullable MutableComponent result;
-    private @Nullable MutableComponent currentLine;
 
-    private void visit(@NotNull Component component, ComponentTextConsumer consumer) {
-        ComponentContents contents = component.getContents();
-
-        if (contents == PlainTextContents.EMPTY) {
-            consumer.accept(Style.EMPTY, null);
-        } else if (contents instanceof PlainTextContents literalContents) {
-            String text = literalContents.text();
-
-            if (text.isEmpty()) {
-                consumer.accept(component.getStyle(), null);
-            } else {
-                consumer.accept(component.getStyle(), text);
-            }
-        } else {
-            throw new IllegalArgumentException("Invalid component type, cannot center component (impossible to guess width): " + contents);
-        }
-
-        for (Component sibling : component.getSiblings()) {
-            this.visit(sibling, consumer);
-        }
-    }
-
-    public @NotNull Component apply(@NotNull Component component) {
-        this.visit(component, (style, text) -> {
-            if (text == null) {
-                this.applyCurrentLine(Component.empty().setStyle(style));
-                return;
-            }
-
-            boolean endsWithNewLine = text.endsWith("\n");
-            String[] lines = text.split("\n");
-
-            if (lines.length > 0) {
-                for (int i = 0; i < lines.length; i++) {
-                    String line = lines[i];
-
-                    if (this.isNotEmpty(this.currentLine, false)) {
-                        this.applyResult();
-
-                        if (endsWithNewLine || i != lines.length - 1) {
-                            line += "\n";
-                        }
-                    }
-
-                    this.applyCurrentLine(Component.literal(line).setStyle(style));
+    public @NotNull net.minecraft.network.chat.Component apply(@NotNull net.minecraft.network.chat.Component input) {
+        net.minecraft.network.chat.Component lastComponent = this.splitAndCollectNewlines(
+                input,
+                input.getStyle(),
+                null,
+                component -> {
+                    this.result = this.setOrAppend(this.result, this.centerSingleLine(component));
                 }
-            } else if (endsWithNewLine) {
-                this.applyCurrentLine(Component.literal("\n"));
-                this.applyResult();
+        );
+
+        if (lastComponent != null) {
+            this.result = this.setOrAppend(this.result, this.centerSingleLine(lastComponent));
+        }
+
+        return Objects.requireNonNullElseGet(this.result, net.minecraft.network.chat.Component::empty);
+    }
+
+    private @Nullable MutableComponent splitAndCollectNewlines(
+            @NotNull net.minecraft.network.chat.Component component,
+            @NotNull Style componentStyle,
+            @Nullable MutableComponent currentComponent,
+            @NotNull Consumer<Component> consumer
+    ) {
+        if (component.getContents() instanceof PlainTextContents contents) {
+            String text = contents.text();
+            String[] lines = this.splitNewline(text);
+
+            for (String line : lines) {
+                currentComponent = this.setOrAppend(currentComponent, Component.literal(line).withStyle(componentStyle).append("\n"));
+
+                consumer.accept(currentComponent);
+                currentComponent = null;
             }
-        });
-
-        if (this.currentLine != null) {
-            this.applyResult();
-        }
-
-        return Objects.requireNonNullElseGet(this.result, Component::empty);
-    }
-
-    private boolean isNotEmpty(@Nullable Component component, boolean countNewlineAsEmpty) {
-        if (component == null) {
-            return false;
-        }
-
-        if (component.getContents() != PlainTextContents.EMPTY) {
-            return !countNewlineAsEmpty ||
-                    !(component.getContents() instanceof PlainTextContents literalContents) ||
-                    !"\n".equals(literalContents.text());
-        }
-
-        return !component.getSiblings().isEmpty();
-    }
-
-    private void applyCurrentLine(@NotNull MutableComponent component) {
-        if (this.currentLine != null) {
-            this.currentLine.append(component);
         } else {
-            this.currentLine = component;
+            currentComponent = this.setOrAppend(currentComponent, MutableComponent.create(component.getContents()).withStyle(componentStyle));
         }
+
+        for (net.minecraft.network.chat.Component sibling : component.getSiblings()) {
+            currentComponent = this.splitAndCollectNewlines(sibling, sibling.getStyle().applyTo(componentStyle), currentComponent, consumer);
+        }
+
+        return currentComponent;
     }
 
-    private void applyResult() {
-        Preconditions.checkNotNull(this.currentLine);
-
-        if (this.isNotEmpty(this.currentLine, true)) {
-            this.currentLine = this.centerSingleLine(this.currentLine);
+    private @NotNull MutableComponent setOrAppend(
+            @Nullable MutableComponent currentComponent,
+            @NotNull MutableComponent toAppend
+    ) {
+        if (currentComponent == null) {
+            return toAppend;
         }
 
-        if (this.result != null) {
-            this.result.append(this.currentLine);
-        } else {
-            this.result = this.currentLine;
-        }
-
-        this.currentLine = null;
+        return currentComponent.append(toAppend);
     }
 
-    private @NotNull MutableComponent centerSingleLine(@NotNull Component component) {
-        float componentHalfWidth = ComponentUtil.widthVanilla(component) / 2.0F;
-        float toCompensate = ComponentUtil.HALF_CHAT_BOX_WIDTH - componentHalfWidth;
+    private @NotNull MutableComponent centerSingleLine(@NotNull net.minecraft.network.chat.Component component) {
+        float componentHalfWidth = ovh.roro.libraries.componentutil.ComponentUtil.widthVanilla(component) / 2.0F;
+        float toCompensate = ovh.roro.libraries.componentutil.ComponentUtil.HALF_CHAT_BOX_WIDTH - componentHalfWidth;
         float spaceWidth = ComponentCenterer.SPACE_WIDTH.getWidth();
         float compensated = 0.0F;
         int spaceCount = 0;
@@ -127,6 +86,27 @@ class ComponentCenterer {
             compensated += spaceWidth;
         }
 
-        return Component.literal(" ".repeat(spaceCount)).append(component);
+        return net.minecraft.network.chat.Component.literal(" ".repeat(spaceCount)).append(component);
+    }
+
+    private @NotNull String @NotNull [] splitNewline(@NotNull String input) {
+        Preconditions.checkNotNull(input);
+
+        List<String> list = new ObjectArrayList<>();
+
+        int index = input.indexOf('\n');
+        int lastIndex = 0;
+        while (index != -1) {
+            list.add(input.substring(lastIndex, index));
+
+            lastIndex = index + 1;
+            index = input.indexOf('\n', index + 1);
+        }
+
+        if (lastIndex != input.length()) {
+            list.add(input.substring(lastIndex));
+        }
+
+        return list.toArray(String[]::new);
     }
 }
